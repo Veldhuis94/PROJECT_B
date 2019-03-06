32e19bbc3c8cafc5514b8f283c92199775268a50
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-03-06 11:57:50 +0100

    yoot
    hello

--- Bastiaan Test Commit.py
+++ Bastiaan Test Commit.py
@@ -1,3 +1,6 @@
 def textify():
     '''prints out REAL info'''
     print('The follwing message has been brought to you by none other than Bastiaan te Veldhuis:\n\nHello and welcome.\nIn this essay, I will explain to you that I have already worked with GitHub in the last project, so I know how it works. All that needs to be done is peer reviewing. Thanks')
+
+textify()
+#yeet
