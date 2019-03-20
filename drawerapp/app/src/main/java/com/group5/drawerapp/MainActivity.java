2c2dc80c19a209835b62d295535940ef47a870cf
Veldhuis94 <43237529+Veldhuis94@users.noreply.github.com>
2019-03-20 10:37:27 +0100

    pud8

--- drawerapp/app/src/main/java/com/group5/drawerapp/MainActivity.java
+++ drawerapp/app/src/main/java/com/group5/drawerapp/MainActivity.java
@@ -73,9 +73,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.showstar) {
             ImageView star = findViewById(R.id.star);
             star.setVisibility(View.VISIBLE);
+        } else if (id == R.id.hidestar) {
+            ImageView star = findViewById(R.id.star);
+            star.setVisibility(View.INVISIBLE);
         }
 
         return super.onOptionsItemSelected(item);
